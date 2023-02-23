8a91f308a7b4770bec0d75d291fc9dafe006b33f
Sapunov <ilsapunov@gmail.com>
2023-02-23 06:30:14 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -35,10 +35,25 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ':  ' ',
 };
 
 function decode(expr) {
     // write your solution here
+    let letters = [];
+    for (let i = 0; i < expr.length / 10; i++) {
+       letters.push(expr.substr(i*10,10));  
+    }
+    for (let i = 0; i < letters.length; i++) {
+        letters[i] = letters[i].replace('**********', " ");
+        letters[i] = letters[i].replace(/00/g, "");
+        letters[i] = letters[i].replace(/10/g, ".");
+        letters[i] = letters[i].replace(/11/g, "-");
+
+        letters[i] = MORSE_TABLE[letters[i]];
+    }
+
+    return letters.join('');
 }
 
 module.exports = {
